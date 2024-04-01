7ba83b7a6ce1612150c40a87eeffa6f2aa88c0a3
mugilan6379 <thiyagarajanaao@gmail.com>
2024-04-01 18:56:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,30 @@
-import time
-import streamlit as st
-import pandas as pd
 import altair as alt
+import pandas as pd
 import seaborn as sns
-
+import streamlit as st
 st.title("Palmer's Penguins")
-with st.spinner():
-    time.sleep(2)
-st.subheader('Use this Streamlit app to make your own scatterplot about penguins!')
-penguins_df=st.file_uploader('Select your local Penguins CSV')
-if penguins_df is not None:
-    penguins_df=pd.read_csv(penguins_df)
+st.markdown("Use this Streamlit app to make your own scatterplot about penguins!")
+selected_x_var = st.selectbox(
+"What do you want the x variable to be?",["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],)
+selected_y_var = st.selectbox(
+"What about the y?", ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],)
+penguin_file = st.file_uploader('Select Your Local Penguins CSV')
+if penguin_file is not None:
+    penguins_df = pd.read_csv(penguin_file)
 else:
-    penguins_df=pd.read_csv('penguins.csv')
-
+    st.stop()
 sns.set_style('darkgrid')
-markers={"Adelie": "X", "Gentoo": "s", "Chinstrap":'o'}
-gender=st.selectbox('Select a gender',['Male','Female','None'])
-penguins_df=penguins_df[penguins_df['sex']==gender]
-selected_species=st.selectbox('What species would you like to visualize?',['Adelie', 'Gentoo', 'Chinstrap'])
+markers = {"Adelie": "X", "Gentoo": "s", "Chinstrap":'o'}
+alt_chart = (
+    alt.Chart(penguins_df, title="Scatterplot of Palmer's Penguins")
+    .mark_circle()
+    .encode(
+        x=selected_x_var,
+        y=selected_y_var,
+        color="species",
+        #color is given for the column we want to color code on graph from the dataset we are upoading
+    )
+    .interactive()
+)
+st.altair_chart(alt_chart, use_container_width=True)
 
-chosen_cloumns=pd.DataFrame(penguins_df.iloc[:,2:6]).columns.to_list()
-#chosen_cloumns_df=chosen_cloumns.to_frame()
-#print(chosen_cloumns)
-x_var=st.selectbox('Choose the x value',chosen_cloumns)
-y_var=st.selectbox('Choose the y value',chosen_cloumns)
-#print(x_var)
-#penguins_df=penguins_df[penguins_df['species']==selected_species]
-
-st.write(penguins_df)
-
-scatter = alt.Chart(penguins_df,title=f'Scatter plot for {selected_species} penguin').mark_circle().encode(
-                x=x_var,
-                y=y_var,
-                tooltip=[x_var,y_var],
-                color='species'
-            ).interactive()
-st.altair_chart(scatter,use_container_width=True)
